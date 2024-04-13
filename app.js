ede1f3aa8bc779ce191803a417dce03b40978bf9
BenjaAM12 <132075096+BenjaAM12@users.noreply.github.com>
2024-04-13 16:02:55 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hola, mundo\n');
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
